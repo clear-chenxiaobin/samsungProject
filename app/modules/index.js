33a4a29355cf1db8562fe3a6fb3d6d5b11112fae
clear-chenxiaobin <chenxiaobin@cleartv.cn>
2016-06-16 14:22:12 +0800

    2016-06-16 样式该为720p

--- app/modules/index.js
+++ app/modules/index.js
@@ -10,7 +10,7 @@ angular.module('app.index', [])
             $scope.welcomeText = '欢迎您来到东方滨江大酒店';
             $scope.subWelcomeText = [
                 '您好！衷心欢迎阁下光临东方滨江大酒店',
-                '我们愿时钟如一的为阁下提供优质的产品和服务',
+                '我们愿始终如一的为阁下提供优质的产品和服务',
                 '使每一位宾客宾至如归，令阁下倍感尊崇之礼遇']
                 .join('\n');
             $scope.roomNumber = '房间号8088';
