13d74dc9737665ac47559f0c47197d39b940ad50
kunmingren <834719922@qq.com>
2016-07-11 13:38:45 +0800

    天气模块

--- app/modules/weather.js
+++ app/modules/weather.js
@@ -1,16 +1,40 @@
 'use strict';
 
 angular.module('app.weather', [])
-    .controller('WeatherController', ['$scope', 'ActivityManager', 'COMMON_KEYS', function ($scope, ActivityManager, COMMON_KEYS) {
+    .controller('WeatherController', ['$scope', 'ActivityManager', 'COMMON_KEYS', 'ResourceManager', '$http', function ($scope, ActivityManager, COMMON_KEYS, ResourceManager, $http) {
         var activity = ActivityManager.getActiveActivity();
         activity.initialize($scope);
 
-        activity.loadI18NResource(function (res) {
-            $scope.title = '天气';
+        //var content = {};
+        var conUrl = ResourceManager.getConfigurations().serverUrl();
+        $scope.conUrl = conUrl;
+        //$scope.serverURl = 'http://172.17.173.100/nativevod/now';
+
+        $http.get(conUrl+'/Weather/local_weather_all.json').success(function (data) {
+            var cityData = ResourceManager.getWeatherCity();
+            console.log(cityData);
+            var weatherData = data.Content;
+            if(cityData){
+                loadWeatherData(cityData.menuNum,cityData.cityNum);
+            }else{
+                loadWeatherData(0,0);
+            }
+            activity.loadI18NResource(function (res) {
+                $scope.title = '天气';
+            });
+            function loadWeatherData(num,cityNum){
+                var loadCity = weatherData[num].Second.Content[cityNum];
+                $scope.content = loadCity;
+            }
+
         });
+
 
         activity.onKeyDown(function (keyCode) {
             switch (keyCode) {
+                case COMMON_KEYS.KEY_ENTER:
+                    ActivityManager.startActivity('tpl_weather_list');
+                    break;
                 case COMMON_KEYS.KEY_BACK:
                     activity.finish();
                     break;
