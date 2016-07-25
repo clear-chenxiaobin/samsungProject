79380e62b0caf04cb200bbefd8abe299e12c439b
kunmingren <834719922@qq.com>
2016-07-25 15:07:05 +0800

    test

--- app/modules/welcome.js
+++ app/modules/welcome.js
@@ -38,7 +38,7 @@ angular.module('app.welcome', [])
                     ResourceManager.setLocale($scope.language);
                     break;
                 case COMMON_KEYS.KEY_ENTER:
-                    ActivityManager.startActivity('index');
+                    ActivityManager.startActivity('test');
                     break;
                 case COMMON_KEYS.KEY_UP:
                     ActivityManager.startActivity('test');
