65fc78a3960cee7b41326a57b3d3b616cf719366
kunmingren <834719922@qq.com>
2016-07-25 16:13:48 +0800

    animation test

--- app/modules/test.js
+++ app/modules/test.js
@@ -5,17 +5,18 @@ angular.module('app.test', [])
         var activity = ActivityManager.getActiveActivity();
         activity.initialize($scope);
 
-        function animate(){
-            var target = document.getElementById('test');
+        $scope.selectedIndex = 0;
+
+        function animate(num){
+            var target = document.getElementById('test').children[num];
             if(hasClass(target,'test_animation')){
-                remove();
-            };
+                remove(num);
+            }
             addClass(target, 'test_animation');
+        }
 
-        };
-
-        function remove(){
-            var target = document.getElementById('test');
+        function remove(num){
+            var target = document.getElementById('test').children[num];
             removeClass(target,'test_animation');
         }
 
@@ -23,7 +24,7 @@ angular.module('app.test', [])
             if (!hasClass(obj, cls)) {
                 obj.className += " " + cls;
             }
-        };
+        }
         function hasClass(obj, cls) {
             return obj.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
         }
@@ -35,6 +36,8 @@ angular.module('app.test', [])
             }
         }
 
+        animate(0);
+
         activity.onKeyDown(function (keyCode) {
             switch (keyCode) {
                 case COMMON_KEYS.KEY_DOWN:
@@ -42,6 +45,20 @@ angular.module('app.test', [])
                     break;
                 case COMMON_KEYS.KEY_UP:
                     animate();
+                    break;
+                case COMMON_KEYS.KEY_LEFT:
+                    if ($scope.selectedIndex > 0) {
+                        $scope.selectedIndex--;
+                    }
+                    remove($scope.selectedIndex+1);
+                    animate($scope.selectedIndex);
+                    break;
+                case COMMON_KEYS.KEY_RIGHT:
+                    if ($scope.selectedIndex < 2) {
+                        $scope.selectedIndex++;
+                    }
+                    remove($scope.selectedIndex-1);
+                    animate($scope.selectedIndex);
                     break;
             }
         })
